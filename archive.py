8cb35b753d78b683c1aea8f9ae67e4cffca5ce0c
Aaron DeVore <aaron.devore@gmail.com>
2012-09-26 14:49:40 -0700

    merge src into top level, fit to new 4chan HTML

--- archive.py
+++ archive.py
@@ -1,111 +1,125 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2010, Aaron DeVore
 # Released under the Don't Be A Douchbag License.
 # Use responsibly. Contribute changes if you feel like it. No CP!
 
-import urllib2
+
+
+import urllib.request, urllib.error
 import os
 import posixpath
 import json
 import time
 from optparse import OptionParser
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 options = OptionParser()
 options.add_option("-b", "--board", dest="board",
-    default='b', help="board name")
-options.add_option("-o", "--overwrite-images", dest="overwriteImages",
+    default='mlp', help="board name")
+options.add_option("-o", "--overwrite-images", dest="overwrite_images",
     default=False, help="Overwrite non-empty images", action="store_true")
 options.add_option("-u", "--update", dest="update", action="store_true",
     default=False, help="update the thread")
 options.add_option("-p", "--pause-update", type="int", dest="pauseUpdate",
     default=100, help="Wait time between thread updates")
-options.add_option("--pause-image", type="int", dest="pauseImage",
+options.add_option("--pause-image", type="int", dest="pause_image",
     default=1, help="Wait time between image downloads")
 
 
 class Post(object):
-    def __init__(self, postID, text, poster, timestamp, image, imageTitle):
-        self.id = postID
-        self.text = text
-        self.poster = poster
-        self.timestamp = timestamp
-        if image:
-            self.image = posixpath.basename(image)
-            self.imageURL = image
-            self.imageTitle = imageTitle
-        else:
-            self.imageURL = self.image = self.imageTitle = None
+    def __init__(self):
+        self.image_name = self.image_location = self.image_original = None
+
+    @property
+    def has_image(self):
+        return self.image_name is not None
+
     def __repr__(self):
-        if self.image:
-            return u"%(id)s by %(poster)s with %(image)s" % self.__dict__
+        if self.image_name:
+            return "%(id)s by %(poster)s with %(image)s" % self.__dict__
         else:
-            return u"%(id)s by %(poster)s with no image" % self.__dict__
+            return "{id} by {poster} with no {image}".format(self.__dict__)
 
 
-def getSoup(board, thread):
+def get_soup(board, thread):
     url = "http://boards.4chan.org/%s/res/%s" % (board, thread)
-    print "downloading thread %s for board %s at %s" % (thread, board, url)
-    f = urllib2.urlopen(url)
+    print("downloading thread %s for board %s at %s" % (thread, board, url))
+    f = urllib.request.urlopen(url)
     soup = BeautifulSoup(f)
     f.close()
     return soup
 
 
-def getPosts(soup):
-    posts = []
-    imageCount = 0
-    postTables = (td.findParent('table')
-        for td in soup.findAll('td', 'doubledash'))
+def parse_posts(soup):
+    image_count = 1  # OP has to have an image
+    op = parse_post(soup.find(True, 'opContainer'))
+    posts = [op]
 
-    for postTable in postTables:
-        postID = postTable.find('td', id=True)['id']
-        text = postTable.find('blockquote').renderContents().strip()
-        posterSpan = postTable.find('span', 'commentpostername')
-        poster = posterSpan.string
-        timestamp = posterSpan.findNext(text=True)
-
-        filespan = postTable.find('span', 'filesize')
-        if filespan:
-            imageCount += 1
-            imageURL = filespan.find('a')['href']
-            imageTitle = postTable.find('span', title=True).string
-        else:
-            imageURL = None
-            imageTitle = None
-
-        post = Post(postID, text, poster, timestamp, imageURL, imageTitle)
-        print u"found %s" % post
-        posts.append(post)
-    print u"found %i posts with %i images" % (len(posts), imageCount)
+    for reply_container in soup.find_all(True, 'replyContainer'):
+        reply = parse_post(reply_container)
+        if reply.image_name:
+            image_count += 1
+        posts.append(reply)
+        
+    print("found {} posts with {} images".format(len(posts), image_count))
     return posts
 
 
-def downloadImages(posts, dest, overwriteImages, pauseImage):
-    imageDir = os.path.join(dest, "images")
-    if not os.path.exists(imageDir):
-        os.mkdir(imageDir)
-    print "pause time between image requests:", pauseImage
+def parse_post(container):
+    p = Post()
+    # id, poster, subject
+    p.id = int(container["id"][2:])
+    p.poster = container.find('span', 'name').text.strip()
+    p.subject = container.find(True, 'subject').text.strip()
+
+    post_number_tag = container.find('span', 'postNum')
+    p.utc = post_number_tag["data-utc"]
+    p.timestamp = post_number_tag.text.strip()
+
+    file_text_tag = container.find(True, 'fileText')
+    if file_text_tag:
+        location_tag = file_text_tag.a
+        p.image_location = "http:" + location_tag["href"]
+        p.image_name = location_tag.text.strip()
+        p.image_original = file_text_tag.span.text.strip()
+
+    post_message_tag = container.find('blockquote', 'postMessage')
+    message = []
+    for child in post_message_tag.children:
+        if isinstance(child, str):
+            message.append(child)
+        elif child.name == 'br':
+            message.append('\n')
+    p.text = ''.join(message)
+
+    return p
+    
+
+def download_images(posts, dest, overwrite_images, pause_image):
+    image_dir = os.path.join(dest, "images")
+    if not os.path.exists(image_dir):
+        os.mkdir(image_dir)
+    print("pause time between image requests:", pause_image)
     for post in posts:
-        if post.image:
-            localPath = os.path.join(imageDir, post.image)
-            if os.path.exists(localPath):
-                if not overwriteImages and os.path.getsize(localPath) != 0:
-                    print u"Skip: image %s already exists" % post.image
+        if post.has_image:
+            local_path = os.path.join(image_dir, post.image_name)
+            if os.path.exists(local_path):
+                if not overwrite_images and os.path.getsize(local_path) != 0:
+                    print("Skip: image %s already exists" % post.image_name)
                     continue
-            print u"downloading %s to %s" % (post.imageURL, post.image)
-            with open(localPath, 'w') as f:
+            print("downloading %s to %s" % (post.image_location, post.image_name))
+            with open(local_path, 'wb') as f:
                 try:
-                    remote = urllib2.urlopen(post.imageURL)
-                except urllib2.HTTPError, e:
+                    remote = urllib.request.urlopen(post.image_location)
+                except urllib.error.HTTPError as e:
                     if e.code == 404:
-                        print "image 404ed"
+                        print("image 404ed")
                     raise
                 f.write(remote.read())
-            time.sleep(pauseImage) # be nice to the servers
+            time.sleep(pause_image) # be nice to the servers
 
 
-def writeData(thread, posts, dest):
+def write_data(thread, posts, dest):
     target = os.path.join(dest, "thread.js")
     jsonPosts = []
     jsonCode = {}
@@ -115,12 +129,16 @@ def writeData(thread, posts, dest):
         jsonPosts.append({
             'id': post.id,
             'poster': post.poster,
-            'image': post.image,
+            'subject': post.subject,
+            'image': {
+                    'name': post.image_name,
+                    'original': post.image_original,
+                    },
             'timestamp': post.timestamp,
+            'utc': post.utc,
             'text': post.text,
-            'imageTitle': post.imageTitle,
             })
-    print u"writing thread data for %s to %s" % (thread, target)
+    print("writing thread data for %s to %s" % (thread, target))
     with open(target, 'w') as f:
         json.dump(jsonCode, f)
 
@@ -128,34 +146,34 @@ def writeData(thread, posts, dest):
 def main():
     opts, args = options.parse_args()
     if len(args) != 2:
-        print options.usage
+        print(options.usage)
     thread = args[0]
     baseDest = args[1]
     board = opts.board
-    overwriteImages = opts.overwriteImages
+    overwrite_images = opts.overwrite_images
     if opts.update:
         updates = -1
     else:
         updates = 1
-    dest = os.path.join(baseDest, u"%s-%s" % (board, thread))
+    dest = os.path.join(baseDest, "%s-%s" % (board, thread))
     if not os.path.exists(dest):
         os.makedirs(dest)
     try:
         while updates != 0:
             updates -= 1
-            soup = getSoup(opts.board, thread)
-            posts = getPosts(soup)
-            downloadImages(posts, dest, overwriteImages, opts.pauseImage)
-            writeData(thread, posts, dest)
+            soup = get_soup(opts.board, thread)
+            posts = parse_posts(soup)
+            download_images(posts, dest, overwrite_images, opts.pause_image)
+            write_data(thread, posts, dest)
             if updates != 0:
-                print "waiting %i seconds for next update" % opts.pauseUpdate
-                print "-" * 40
+                print("waiting %i seconds for next update" % opts.pauseUpdate)
+                print("-" * 40)
                 time.sleep(opts.pauseUpdate)
     except KeyboardInterrupt:
-        print "Keyboard Interrupt, ending archiving"
-    except urllib2.HTTPError, e:
+        print("Keyboard Interrupt, ending archiving")
+    except urllib.error.HTTPError as e:
         if e.code == 404:
-            print "Thread or image 404ed"
+            print("Thread or image 404ed")
         else:
             raise
 
